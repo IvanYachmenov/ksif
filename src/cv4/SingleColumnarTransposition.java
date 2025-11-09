7865d8ef59305d7c22336b1547450543f94eee72
Ivan Yachmenov <obmenakkount@gmail.com>
2025-11-09 05:28:02 +0100

    added implementation of Transposotional cipher + SingleColumnarTransposition

--- src/cv4/SingleColumnarTransposition.java
+++ src/cv4/SingleColumnarTransposition.java
@@ -1,4 +1,94 @@
 package cv4;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static cv3.PermutationFromPhrase.permutationFromPhrase;
+
 public class SingleColumnarTransposition {
+    Integer[] key;
+
+    private SingleColumnarTransposition(){}
+
+    public SingleColumnarTransposition(Integer[] key){
+        this.key = key;
+    }
+
+    public SingleColumnarTransposition(String key){
+        this.key = permutationFromPhrase(key);
+    }
+
+    public String encrypt(String plainText) {
+        int columns = key.length;
+        int rows = plainText.length() / columns;
+        if(plainText.length() % columns > 0) {
+            rows++;
+        }
+
+        char[][] matrix = new char[rows][columns];
+        int i = 0;
+        int j = 0;
+        for(char c : plainText.toCharArray()){
+            matrix[i][j++] = c;
+            if(j == columns){
+                i++;
+            }
+            j %= columns;
+        }
+
+        List<Integer> permList = new ArrayList<>();
+        for(int c = 0; c < columns; c++){
+            permList.add(key[c]);
+        }
+
+        StringBuilder toReturn = new StringBuilder();
+        for(int col = 0; col < columns; col++){
+            int colIdx = permList.indexOf(col);
+            for(int row = 0; row < rows; row++){
+                if(matrix[row][colIdx] != '\u0000'){
+                    toReturn.append(matrix[row][colIdx]);
+                }
+            }
+        }
+
+        return toReturn.toString();
+    }
+
+    public String decrypt(String cipherText) {
+        int columns = key.length;
+        int rows = cipherText.length() / columns;
+        int nonCompleteCols = cipherText.length() % columns;
+        if(nonCompleteCols > 0){
+            rows++;
+        }
+        List<Integer> permList = new ArrayList<>();
+        for(int pos = 0; pos < key.length; pos++){
+            permList.add(key[pos]);
+        }
+
+        char[][] matrix = new char[rows][columns];
+        int idx = 0;
+        for(int col = 0; col < columns; col++){
+            int colIdx = permList.indexOf(col);
+            int maxRowIdx = rows;
+
+            if(nonCompleteCols > 0 && (colIdx >= nonCompleteCols)){
+                maxRowIdx--;
+            }
+
+            for(int row = 0; row < maxRowIdx; row++){
+                matrix[row][colIdx] = cipherText.charAt(idx++);
+            }
+        }
+
+        StringBuilder toReturn = new StringBuilder();
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < columns; col++){
+                if(matrix[row][col] != '\u0000'){
+                    toReturn.append(matrix[row][col]);
+                }
+            }
+        }
+        return toReturn.toString();
+    }
 }
--- src/cv4/Transposition.java
+++ src/cv4/Transposition.java
@@ -1,4 +1,59 @@
 package cv4;
 
+import static cv3.PermutationFromPhrase.permutationFromPhrase;
+import static cv3.InversionPermutation.inverse;
+
+
 public class Transposition {
+    Integer[] key;
+    Integer[] inverseKey;
+
+    private Transposition() {}
+
+    public Transposition(Integer[] key) {
+        this.key = key;
+        this.inverseKey = inverse(key);
+    }
+
+    public Transposition(String key_) {
+        this.key = permutationFromPhrase(key_);
+    }
+
+    public String encrypt(String plainText) {
+        if(plainText.length() % key.length != 0) {
+            int toFill = key.length - (plainText.length() % key.length);
+            StringBuilder plainTextBuilder = new StringBuilder(plainText);
+            for(int i = 0; i < toFill; i++) {
+                plainTextBuilder.append('x');
+            }
+            plainText = plainTextBuilder.toString();
+        }
+        return crypt(plainText, key);
+    }
+
+    public String decrypt(String cipherText) {
+        return crypt(cipherText, inverseKey);
+    }
+
+
+    public String crypt(String plainText, Integer[] key) {
+        int block_len = key.length;
+        int blocks = plainText.length() / block_len;
+        StringBuilder cryptedText = new StringBuilder();
+        for(int i = 0; i < blocks; i++) {
+            int idx =  i * block_len;
+            String substring = plainText.substring(idx, idx + block_len);
+            String reordered = applyPermutaion(substring, key);
+            cryptedText.append(reordered);
+        }
+        return cryptedText.toString();
+    }
+
+    public static String applyPermutaion(String substring, Integer[] perm) {
+        char[] output = new char[perm.length];
+        for(int i = 0; i < perm.length; i++) {
+            output[perm[i]] =  substring.charAt(i);
+        }
+        return new String(output);
+    }
 }
