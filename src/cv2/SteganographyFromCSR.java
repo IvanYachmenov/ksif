b74198f9157db20798d6cd62834b2525689a191e
Ivan Yachmenov <obmenakkount@gmail.com>
2025-11-09 04:44:31 +0100

    steganography implementation added

--- src/cv2/SteganographyFromCSR.java
+++ src/cv2/SteganographyFromCSR.java
@@ -1,4 +1,81 @@
 package cv2;
 
+import java.util.Random;
+
 public class SteganographyFromCSR {
+
+    public static final String TSA = " abcdefghijklmnopqrstuvwxyz";
+    public static final Random rnd = new Random(System.currentTimeMillis());
+
+    public static String writeEven(String plainText) {
+        StringBuilder cipherText = new  StringBuilder();
+        plainText = plainText.replaceAll(" ", "");
+        int counter = 0;
+        for(char x : plainText.toCharArray()) {
+            counter++;
+            char[] tmp = new char[3];
+            tmp[0] = TSA.charAt(rnd.nextInt(TSA.length()));
+            tmp[1] = x;
+            tmp[2] = TSA.charAt(rnd.nextInt(TSA.length()));
+            cipherText.append(tmp);
+            if(counter % 2 == 0) {
+                cipherText.append(" ");
+            }
+        }
+        return cipherText.toString().trim();
+    }
+
+    public static String readEven(String cipherText) {
+        StringBuilder plainText = new  StringBuilder();
+        cipherText = cipherText.replaceAll(" ", "");
+        for(int i = 0; i < cipherText.length(); i+=3) {
+            plainText.append(cipherText.charAt(i+1));
+        }
+        return plainText.toString();
+    }
+
+    public static String writeOdd(String plainText) {
+        StringBuilder cipherText = new  StringBuilder();
+        String[] words = plainText.split(" ");
+        int counter = 0;
+        for(String word : words) {
+            StringBuilder wrd = new StringBuilder(word);
+            wrd.reverse();
+            for(char x : wrd.toString().toCharArray()) {
+                counter++;
+                char[] tmp = new char[3];
+                tmp[0] = TSA.charAt(rnd.nextInt(TSA.length()));
+                tmp[1] = x;
+                tmp[2] = TSA.charAt(rnd.nextInt(TSA.length()));
+                cipherText.append(tmp);
+                if(counter % 2 == 0) {
+                    cipherText.append(" ");
+                }
+            }
+            char div = TSA.charAt(rnd.nextInt(TSA.length()));
+            cipherText.append(" ").append(div).append(" ");
+        }
+        return cipherText.toString().trim();
+    }
+
+    public static String readOdd(String cipherText) {
+        cipherText = cipherText.replaceAll(" +"," ");
+        StringBuilder plainText = new  StringBuilder();
+        String[] divs = cipherText.split(" ");
+        StringBuilder tmp = new StringBuilder();
+        for(String d : divs) {
+            if(d.length() % 3 == 0) {
+                char x1 = d.charAt(1);
+                tmp.append(x1);
+
+                if(d.length() % 6 == 0) {
+                    char x2 = d.charAt(4);
+                    tmp.append(x2);
+                }
+            } else {
+                plainText.append(tmp.reverse()).append(" ");
+            }
+        }
+        return plainText.toString();
+    }
 }
--- src/cv2/SteganographyInWords.java
+++ src/cv2/SteganographyInWords.java
@@ -1,4 +1,46 @@
 package cv2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
 public class SteganographyInWords {
+
+    static final Random rnd = new Random(System.currentTimeMillis());
+
+    Map<Character, List<String>> dict;
+
+    public SteganographyInWords(String dictPath) throws IOException {
+        List<String> words = Files.readAllLines(Paths.get(dictPath));
+        dict = new HashMap<>();
+        for(String wrd : words) {
+            Character fst = wrd.charAt(0);
+            if(!dict.containsKey(fst)) {
+                dict.put(fst, new ArrayList<>());
+            }
+            dict.get(fst).add(wrd);
+        }
+    }
+
+    public String write(String plainText) {
+        StringBuilder cipherText = new StringBuilder();
+        for(char x : plainText.toCharArray()) {
+            if(dict.containsKey(x)) {
+                List<String> words = dict.get(x);
+                String rndWord = words.get(rnd.nextInt(words.size()));
+                cipherText.append(rndWord).append(" ");
+            }
+        }
+        return cipherText.toString().trim();
+    }
+
+    public String read(String cipherText) {
+        StringBuilder plainText = new StringBuilder();
+        String[] words = cipherText.split(" ");
+        for(String wrd : words) {
+            plainText.append(wrd.charAt(0));
+        }
+        return plainText.toString();
+    }
 }
